d621d2004afdf682d0e61cd4534742dd87439cda
Maurice Yang <mauriceyangmy28@gmail.com>
2024-05-08 00:56:00 +0700

    “Experiment 1.3: Multiple Spawn and removing drop

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ![Reflection 1](image.png)
 Hasil dari kode ini disebabkan pada waktu kapan fungsi tersebut dijalankan. Pada saat fungsi `main()` dijalankan, semua fungsi sinkronus akan dijalankan terlebih dahulu (Print "hey hey") lalu fungsi asinkronus akan dijalankan. 
+![Reflection 2](image-1.png)
+Fungsi `drop()` memberi tahu kepada executor bahwa tidak ada lagi task yang akan dijalankan. Sehingga executor tidak akan menunggu semua task yang ada selesai dijalankan. Karena fungsi `drop()` tidak dijalankan pada contoh ini, maka executor akan menganggap masih task yang belum selesai dijalankan.
--- src/main.rs
+++ src/main.rs
@@ -104,11 +104,25 @@ fn main() {
         println!("Maurice's Computer: done!");
     });
 
+    spawner.spawn(async {
+        println!("Maurice's Computer: howdy2!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Maurice's Computer: done2!");
+    });
+
+    spawner.spawn(async {
+        println!("Maurice's Computer: howdy3!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Maurice's Computer: done3!");
+    });
+
     println!("Maurice's Computer: hey hey...");
 
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
-    drop(spawner);
+    // drop(spawner);
 
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".

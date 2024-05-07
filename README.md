a6f840c54765a9aab3404d6ed9c39115cfaf0e7a
Maurice Yang <mauriceyangmy28@gmail.com>
2024-05-08 00:42:19 +0700

    Experiment 1.2: Understanding how it works

---
+++ README.md
@@ -0,0 +1,2 @@
+![Reflection 1](image.png)
+Hasil dari kode ini disebabkan pada waktu kapan fungsi tersebut dijalankan. Pada saat fungsi `main()` dijalankan, semua fungsi sinkronus akan dijalankan terlebih dahulu (Print "hey hey") lalu fungsi asinkronus akan dijalankan. 
--- src/main.rs
+++ src/main.rs
@@ -104,6 +104,8 @@ fn main() {
         println!("Maurice's Computer: done!");
     });
 
+    println!("Maurice's Computer: hey hey...");
+
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
     drop(spawner);
